37e5f9403cda31ead06000934c83e0d81814c008
MostafaGadoo <73221261+MostafaGadoo@users.noreply.github.com>
2022-05-03 14:32:56 +0200

    Database separate files

---
+++ OnlineFoodOrderingSystem/src/onlinefoodorderingsystem/DB_Connection_Gado.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package onlinefoodorderingsystem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author mostafa gado
+ */
+public class DB_Connection_Gado {
+     private final String userName = "root";
+    private final String password = "";
+    private final String dbName = "food_ordering_system";
+
+    private Connection con;
+
+     public DB_Connection_Gado(){
+        try {
+            //Loading the jdbc driver
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            //Get a connection to database
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dbName, userName, password);
+        } catch (Exception e) {
+            System.err.println("DATABASE CONNECTION ERROR: " + e.toString());
+        }
+    }
+}
